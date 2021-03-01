99a7d4e2e427d870d098ebf094447b8c99ea608c
rafaelpllopes <rafael.pllopes@gmail.com>
2021-03-01 07:23:11 -0300

    Providencia para receber, todos, por local ou por rep.

--- inserir_registros.py
+++ inserir_registros.py
@@ -4,6 +4,7 @@ from json import load
 from datetime import date
 from inserir_db import InserirDB
 from helpers.remove_files import RemoveFiles
+from sys import argv
 
 def registradores():
     registradores = []
@@ -13,7 +14,7 @@ def registradores():
     
     return registradores
 
-def main():
+def inserir_todos():
     for registrador in registradores():
         try:
             rep = registrador['rep']
@@ -29,6 +30,28 @@ def main():
             print(error)
     
     RemoveFiles.remove_all()
-    
+
+def inserir_por_local(local):
+    for registrador in registradores():
+        if local.lower() in registrador['local'].lower() or local == registrador['rep']:
+            try:
+                rep = registrador['rep']
+                ponto = registrador['codigo_db']
+                inserir = InserirDB(rep, ponto)
+                
+                data_atual = date.today()
+                mes = str(data_atual.month).zfill(2)
+                ano = str(data_atual.year)
+                
+                inserir.inserir_registros(mes, ano)
+            except Exception as error:
+                print(error)
+            break
+                
 if __name__ == '__main__':
-    main()
+    try: 
+        local = argv[1]
+        inserir_por_local(local)
+    except:
+        pass
+        inserir_todos()
